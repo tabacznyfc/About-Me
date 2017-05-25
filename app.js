9a66063202e9717746893c6bf95482951a1cf838
tabacznyfc <tabacznyfchristian@icloud.com>
2017-05-24 21:33:51 -0700

    intial commit with project scaffolding

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var confirmed = comfrim('Are you ready to rock and roll!?');
+if (confirmed === true){
+  console.log('The user is ready');
+
+}else{
+  console.log('The user ssaid they were not ready...');
+}
