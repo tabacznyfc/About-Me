f4fb266e0d0e8df734f90dfd179151284846f993
tabacznyfc <tabacznyfchristian@icloud.com>
2017-06-01 21:38:23 -0700

    second commit created questions

--- app.js
+++ app.js
@@ -1,9 +1,49 @@
-'use strict';
+'use strict'
+var myAnswer1= 'no'
+var myAnswer2 ='YES'
+var myAnswer3 ='yes'
+var myAnswer4 ='no'
+var myAnswer5 ='YES'
 
-var confirmed = comfrim('Are you ready to rock and roll!?');
-if (confirmed === true){
-  console.log('The user is ready');
+var userName = prompt('Hello what is your name?');
 
-}else{
-  console.log('The user ssaid they were not ready...');
-}
+function guessingGame(){
+  var question1 = prompt('Am I over thirty?');
+  if(question1.toLowerCase() === myAnswer1){
+    alert('Yes Correct!');
+  }
+  else{
+    alert('Sorry wrong!');
+  }
+  var question2 = prompt('Do I want a unicorn?');
+  if(question2.toUpperCase() === myAnswer2){
+    alert('Woohoo you got it correct!');
+    }
+    else{
+      alert('Sorry wrong!');
+    }
+    var question3 =prompt('Have I ever skipped school?');
+    if(question3.toLowerCase() === myAnswer3){
+      alert('Correct!');
+    }
+    else{
+      alert('Sorry wrong!');
+    }
+    var question4 =prompt('Have I ever forgot my anniversary?');
+    if (question4.toUpperCase()=== myAnswer4){
+      alert('Can\t never forget that');
+    }
+    else{
+      alert('Would be crazy to forget such thing lol!');
+    }
+    var question5 =prompt('Do I think about the future?')
+    if (question5.toLowerCase()=== myAnswer5){
+        alert('Yes I do!');
+    }
+    else{
+      alert('Who does not think about the future');
+    }
+  }
+
+
+guessingGame();
