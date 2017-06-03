239697d7fcb79b40cb3abdda80c59d0e9790779d
tabacznyfc <tabacznyfchristian@icloud.com>
2017-06-03 13:20:03 -0700

    updated js file

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 'use strict'
-var myAnswer1= 'no'
-var myAnswer2 ='YES'
-var myAnswer3 ='yes'
-var myAnswer4 ='no'
-var myAnswer5 ='YES'
+var myAnswer1 = 'no'
+var myAnswer2 = 'YES'
+var myAnswer3 = 'yes'
+var myAnswer4 = 'no'
+var myAnswer5 = 'YES'
+var myAnswer6 = 'yes'
 
 var userName = prompt('Hello what is your name?');
+alert('Hello ' + userName);
+
+var game = prompt('Would you like to play a game? y/n');
+if ((game === 'y') || (game === 'yes') || (game === 'n') ||(game === 'no')){
+  alert('Let\s play');
+}
+else{
+  alert('Ok let\s not');
+}
+
 
 function guessingGame(){
   var question1 = prompt('Am I over thirty?');
@@ -22,26 +33,33 @@ function guessingGame(){
     else{
       alert('Sorry wrong!');
     }
-    var question3 =prompt('Have I ever skipped school?');
+    var question3 = prompt('Have I ever skipped school?');
     if(question3.toLowerCase() === myAnswer3){
       alert('Correct!');
     }
     else{
       alert('Sorry wrong!');
     }
-    var question4 =prompt('Have I ever forgot my anniversary?');
-    if (question4.toUpperCase()=== myAnswer4){
+    var question4 = prompt('Have I ever forgot my anniversary?');
+    if (question4.toUpperCase() === myAnswer4){
       alert('Can\t never forget that');
     }
     else{
       alert('Would be crazy to forget such thing lol!');
     }
-    var question5 =prompt('Do I think about the future?')
-    if (question5.toLowerCase()=== myAnswer5){
+    var question5 = prompt('Do I think about the future?')
+    if (question5.toLowerCase() === myAnswer5){
         alert('Yes I do!');
     }
     else{
       alert('Who does not think about the future');
+    }
+    var question6 = prompt('Do I like pizza?');
+    if (question6.toUpperCase() === myAnswer6){
+      alert('Correct');
+    }
+    else{
+      alert('Who does not like pizza');
     }
   }
 
