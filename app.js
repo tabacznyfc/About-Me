ef9c9a7e094ad1535e9d541b48596d5797ec6b23
krichey15 <richey.kyle.i@gmail.com>
2017-06-03 16:33:49 -0700

    fixed syntax

--- app.js
+++ app.js
@@ -20,47 +20,51 @@ else {
 }
 
 var question1 = prompt('Am I over thirty?');
-  if(question1.toLowerCase() === myAnswer1){
-    alert('Yes Correct!');
-  }
-  else{
-    alert('Sorry wrong!');
-  }
-  var question2 = prompt('Do I want a unicorn?');
-  if(question2.toUpperCase() === myAnswer2){
-    alert('Woohoo you got it correct!');
-    }
-    else{
-      alert('Sorry wrong!');
-    }
-    var question3 = prompt('Have I ever skipped school?');
-    if(question3.toLowerCase() === myAnswer3){
-      alert('Correct!');
-    }
-    else{
-      alert('Sorry wrong!');
-    }
-    var question4 = prompt('Have I ever forgot my anniversary?');
-    if (question4.toUpperCase() === myAnswer4){
-      alert('Can\t never forget that');
-    }
-    else{
-      alert('Would be crazy to forget such thing lol!');
-    }
-    var question5 = prompt('Do I think about the future?')
-    if (question5.toLowerCase() === myAnswer5){
-        alert('Yes I do!');
-    }
-    else{
-      alert('Who does not think about the future');
-    }
-    var question6 = prompt('Do I like pizza?');
-    if (question6.toUpperCase() === myAnswer6){
-      alert('Correct');
-    }
-    else{
-      alert('Who does not like pizza');
-    }
-  }
+
+if(question1.toLowerCase() === myAnswer1){
+  alert('Yes Correct!');
+} else{
+  alert('Sorry wrong!');
+}
+
+var question2 = prompt('Do I want a unicorn?');
+
+if(question2.toUpperCase() === myAnswer2){
+  alert('Woohoo you got it correct!');
+} else{
+  alert('Sorry wrong!');
+}
+
+var question3 = prompt('Have I ever skipped school?');
+
+if(question3.toLowerCase() === myAnswer3){
+  alert('Correct!');
+} else{
+  alert('Sorry wrong!');
+}
+
+var question4 = prompt('Have I ever forgot my anniversary?');
+
+if (question4.toUpperCase() === myAnswer4){
+  alert('Can\t never forget that');
+} else{
+  alert('Would be crazy to forget such thing lol!');
+}
+
+var question5 = prompt('Do I think about the future?')
+if (question5.toLowerCase() === myAnswer5){
+    alert('Yes I do!');
+} else{
+  alert('Who does not think about the future');
+}
+
+var question6 = prompt('Do I like pizza?');
+
+if (question6.toUpperCase() === myAnswer6){
+  alert('Correct');
+} else{
+  alert('Who does not like pizza');
+}
+
 
 gameTime();
